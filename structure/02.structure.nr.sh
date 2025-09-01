fd9c4ef144f8cf2709e222ff7d5e682bca51d523
Ira Cooke <iracooke@gmail.com>
2025-09-01 15:44:03 +1000

    Fix sample truncation in remove clones

--- structure/02.structure.nr.sh
+++ structure/02.structure.nr.sh
@@ -1,10 +1,14 @@
 # Full structure analysis on filtered dataset
 
 for k in 1 2 3 4;do
-	structure -K ${k} -L 5269 -N 436  -m ak.mainparams.nr.txt -i ak.filtered.nr.str -o ak.structure.nr.k${k}.out
+	structure -K ${k} -L 5265 -N 427  -m ak.mainparams.nr.txt -i ak.filtered.nr.str -o ak.structure.nr.k${k}.out
 	#Extract ancestries
-	grep -A 437 'Inferred ancestry of individuals' ak.structure.nr.k${k}.out_f | sed 's/://'> ak.structure.nr.k${k}.out.anc.txt
+	grep -A 428 'Inferred ancestry of individuals' ak.structure.nr.k${k}.out_f | sed 's/://'> ak.structure.nr.k${k}.out.anc.txt
 done
 
 #Write samples in order for later reading
-cat ak.filtered.nr.str| awk '{print $1}' | tail -n+2 | uniq > ak.filtered.samples.txt
+cat ak.filtered.nr.str| awk '{print $1}' | tail -n+2 | uniq > ak.filtered.nr.samples.txt
+
+# Summarise likelihoods
+
+grep 'Prob of Data' *.nr.* | awk '{print $1,$NF}' | sed -E 's/.*k([1234]).* (.*)/\1 \2/' > k_vs_lik.txt
