2ee3b92cdcbf6b20492a8d114eec2b8400e1b3d5
Ira Cooke <iracooke@gmail.com>
2026-08-25 15:13:05 +1000

    Add structure shell scripts

--- structure/02.structure.nr.sh
+++ structure/02.structure.nr.sh
@@ -7,9 +7,9 @@ run_structure(){
 	mkdir -p nr.${rep}
 	cd nr.${rep}
 
-	structure -K ${k} -L 5267 -N 426  -m ../ak.mainparams.nr.txt -i ../ak.filtered.nr.str -e ../extraparams -o ak.structure.nr.k${k}.out
+	structure -K ${k} -L 5309 -N 427  -m ../ak.mainparams.nr.txt -i ../ak.filtered.nr.str -e ../extraparams -o ak.structure.nr.k${k}.out
 	#Extract ancestries
-	grep -A 427 'Inferred ancestry of individuals' ak.structure.nr.k${k}.out_f | sed 's/://'> ak.structure.nr.k${k}.out.anc.txt
+	grep -A 428 'Inferred ancestry of individuals' ak.structure.nr.k${k}.out_f | sed 's/://'> ak.structure.nr.k${k}.out.anc.txt
 
 	cd ../
 }
